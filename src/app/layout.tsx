a191512dcdaff9436741e398e5fd0a71846aa7f1
kjh302903 <kjh302903@gmail.com>
2025-07-29 09:03:32 +0900

    feat: 폰트 적용
    나눔 고딕 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nanum_Gothic } from "next/font/google";
 import "./globals.css";
+import GlobalNavigationBar from "./_component/GlobalNavigationBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const nanumGothic = Nanum_Gothic({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${nanumGothic.className} antialiased`}>
+        <GlobalNavigationBar />
         {children}
       </body>
     </html>
